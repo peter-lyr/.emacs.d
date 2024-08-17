88b04858643a803ee07d2f0521711e5e96d1f4e5
peter-lyr <llydrp.ldp@gmail.com>
2024-08-17 23:54:13 +0800

    ;; Open org link in the same window

--- lisp/init-startup.el
+++ lisp/init-startup.el
@@ -53,4 +53,12 @@
 ;; 报错日志
 (setq debug-on-error t)
 
+;; Open org link in the same window
+(setq org-link-frame-setup
+      '((vm . vm-visit-folder-other-frame)
+        (vm-imap . vm-visit-imap-folder-other-frame)
+        (gnus . org-gnus-no-new-news)
+        (file . find-file)
+        (wl . wl-other-frame)))
+
 (provide 'init-startup)
