b879a0e2b8783a84157b1f18a5d16941c49303d2
peter-lyr <llydrp.ldp@gmail.com>
2024-08-17 22:19:54 +0800

    ;; (setq evil-want-integration t) ;; This is optional since it's already set to t by default.

--- init.el
+++ init.el
@@ -21,7 +21,7 @@
     (use-package evil
                  :ensure t
                  :init
-                 (setq evil-want-integration t) ;; This is optional since it's already set to t by default.
+                 ;; (setq evil-want-integration t) ;; This is optional since it's already set to t by default.
                  (setq evil-want-keybinding nil)
                  :config
                  (evil-mode 1))
