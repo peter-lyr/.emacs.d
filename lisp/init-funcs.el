77bc7a4fd6b7b2374bc5abd370a3c5974b58b0d9
peter-lyr <llydrp.ldp@gmail.com>
2024-08-18 12:30:51 +0800

    (defun copy-buffer-file-name (choice)

--- lisp/init-funcs.el
+++ lisp/init-funcs.el
@@ -33,14 +33,27 @@
   (package-refresh-contents)
   )
 
-(defun copy-buffer-file-name ()
-  "Copy the full path to the current file and show in the minibuffer."
-  (interactive)
-  (let ((file-name (buffer-file-name)))
-    (if file-name
-      (progn
-        (message file-name)
-        (kill-new file-name))
-      (error "Buffer not visiting a file"))))
+(defun copy-buffer-file-name (choice)
+  "Copyies the buffer {name/mode}, file {name/full path/directory} to the kill-ring.
+  See https://stackoverflow.com/questions/2416655/file-path-to-clipboard-in-emacs"
+  (interactive "cCopy (b) buffer name, (m) buffer major mode, (f) full buffer-file path, (d) buffer-file directory, (n) buffer-file basename")
+  (let ((new-kill-string)
+        (name (if (eq major-mode 'dired-mode)
+                (dired-get-filename)
+                (or (buffer-file-name) ""))))
+    (cond ((eq choice ?f)
+           (setq new-kill-string name))
+          ((eq choice ?d)
+           (setq new-kill-string (file-name-directory name)))
+          ((eq choice ?n)
+           (setq new-kill-string (file-name-nondirectory name)))
+          ((eq choice ?b)
+           (setq new-kill-string (buffer-name)))
+          ((eq choice ?m)
+           (setq new-kill-string (format "%s" major-mode)))
+          (t (message "Quit")))
+    (when new-kill-string
+      (message "%s copied" new-kill-string)
+      (kill-new new-kill-string))))
 
 (provide 'init-funcs)
