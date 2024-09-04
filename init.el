efe24a936034f405fca9c280562d276dc8eb4a55
peter-lyr <llydrp.ldp@gmail.com>
2024-09-04 23:26:24 +0800

    ('melpa-stable' . 'https://stable.melpa.org/packages/')

--- init.el
+++ init.el
@@ -1,5 +1,6 @@
 (require 'package)
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("melpa-stable" . "https://stable.melpa.org/packages/")
                          ("org" . "https://orgmode.org/elpa/")
                          ("gnu" . "https://elpa.gnu.org/packages/")))
 (package-initialize)
