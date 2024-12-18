343cc80497d2b41281d0fb6c67900837d55a230d
peter-lyr <llydrp.ldp@gmail.com>
2024-12-18 17:45:39 +0800

    (evil-leader/set-key "fk" 'open-work-summary-week-org)

--- init.el
+++ init.el
@@ -193,7 +193,7 @@
 ;; 打开org文件
 (evil-leader/set-key "fi" 'open-init-org)
 (evil-leader/set-key "fw" 'open-work-org)
-(evil-leader/set-key "fe" 'open-work-summary-week-org)
+(evil-leader/set-key "fk" 'open-work-summary-week-org)
 (evil-leader/set-key "fa" 'open-work-summary-day-org)
 ;; org journal
 (evil-leader/set-key "n j SPC" 'org-roam-dailies-goto-today)
